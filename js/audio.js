b3b5b921fa664cf526794e7d62fd4125f7989e6c
Malik nayab afsar <71265622+Nayabacademic@users.noreply.github.com>
2021-06-05 15:48:50 +0530

    Update/add

---
+++ js/audio.js
@@ -0,0 +1,34 @@
+let jumpSound;
+let coinSound;
+let hitSound;
+let backSound;
+let clickSound;
+let magnetSound;
+
+function sound(src, loopV, vol) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    if (isNaN(vol)) { 
+      vol = .1; 
+    } 
+    this.sound.volume = parseFloat(vol)
+    this.sound.loop = loopV;
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+      }
+    }
+    
+
+jumpSound = new sound("audio/jump.mp3", false, 1)
+coinSound = new sound("audio/coin.mp3", false, 1)
+hitSound = new sound("audio/hit.ogg", false)
+clickSound = new sound("audio/btnClick.ogg", false, 1)
+backSound = new sound("audio/backLoop.wav", true, 0.28)
+magnetSound = new sound("audio/magnet.ogg", false, 1)
--- js/script.js
+++ js/script.js
@@ -27,6 +27,10 @@ var screenH = window.innerHeight/canvas.height
 canvas.style.width = canvas.width * screenW + "px"
 canvas.style.height = canvas.height * screenH +"px"
 
+//AUDIO_________\\
+ 
+
+
 //FIREBASE___________________\\
 
 var config = {
@@ -139,19 +143,7 @@ var gameEnd = false;
 var add = 0
 
 
-let jumpSound;
-let coinSound;
-let hitSound;
-let backSound;
-let clickSound;
-let magnetSound;
 
-jumpSound = new sound("audio/jump.mp3", false, 1)
-coinSound = new sound("audio/coin.mp3", false, 1)
-hitSound = new sound("audio/hit.ogg", false)
-clickSound = new sound("audio/btnClick.ogg", false, 1)
-backSound = new sound("audio/backLoop.wav", true, 1)
-magnetSound = new sound("audio/magnet.ogg", false, 1)
  
 // OBJECTS_________________________________\\
 
@@ -374,26 +366,7 @@ class Magnet{
 	}
 }
     
- function sound(src, loopV, vol) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    if (isNaN(vol)) { 
-      vol = .1; 
-    } 
-    this.sound.volume = parseFloat(vol)
-    this.sound.loop = loopV;
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
-    }
-    this.stop = function(){
-        this.sound.pause();
-      }
-    }
-    
+
  
 function disToast(word){
  
